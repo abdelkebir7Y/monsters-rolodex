e0e94909686099ec7857eddc09a4ba25064e9fa6
abdelkebir7Y <abdouhassou92@gmail.com>
2021-03-01 22:38:57 +0100

    adding files for github pages

---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from '../card/card.component';
+import './card-list.styles.css';
+
+export const CardLidt = (props) => {
+    return (
+        <div className='card-list'>  
+          {
+            props.monsters.map( monster => <Card key = {monster.id} monster={monster} /> )
+          }
+        </div>
+    );
+}
